ef9c57bb5e4b8607895856b2d64787a5e1f33366
Gabriel Colón <gabriel.colon32@upr.edu>
2025-05-21 23:37:49 -0400

    removed print

--- fetchStationData.js
+++ fetchStationData.js
@@ -126,7 +126,6 @@ function parseCSV(csvText, fileName) {
     return parseFloat(columns[rainIndex]) || 0;
   });
 
-  console.log(fileName, rainValues);
   const totalRain = rainValues.reduce((acc, val) => acc + val, 0);
   fetchedStationData[stationName]["12hr_rain_mm_total"] = totalRain.toFixed(2);
 
